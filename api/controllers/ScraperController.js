9e4a842998e8ec7a624a7946006a79209445a5dd
alincode <alincode@gmail.com>
2016-11-04 14:38:19 +0800

    try insltall extra module

--- api/controllers/ScraperController.js
+++ api/controllers/ScraperController.js
@@ -1,3 +1,5 @@
+// var digikey = require('digikey-scraper-sandbox');
+
 // ScraperController
 
 module.exports = {
@@ -12,6 +14,10 @@ module.exports = {
 	},
 	scraper: async(req, res) => {
 		try {
+			// let result = await digikey.product({
+			// 	url: 'http://www.digikey.tw/product-detail/zh/comchip-technology/ZENER-KIT/641-1426-ND/2217259'
+			// });
+			// console.log("==========> result:", result);
 			return res.ok();
 		} catch (e) {
 			return res.serverError(e);
