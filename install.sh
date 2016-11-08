14959227fa9cc1cb67c88b58525ab18d865c39ac
alincode <alincode@gmail.com>
2016-11-08 17:06:53 +0800

    backup install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+nvm install 4.6.0
+yum install git
+yum whatprovides netstat
+yum install net-tools
+yum install mongodb-org
+systemctl start mongod
+netstat -tnlp
