7a399ca0b79e4a9dc32a828449509ad5b63bf96e
alincode <alincode@gmail.com>
2016-11-08 16:50:13 +0800

    catch parse error and adjust get product api

--- api/controllers/ParserController.js
+++ api/controllers/ParserController.js
@@ -1,33 +1,55 @@
 // ParserController
 module.exports = {
-	query: async(req, res) => {
+	find: async(req, res) => {
 		let data = req.body;
 
 		try {
-			let results = [];
-			let result = await DigikeyScraper.getResult(null,
-				'http://www.digikey.tw/product-detail/zh/comchip-technology/ZENER-KIT/641-1426-ND/2217259'
-			);
-			results.push(result);
+			// let result = await DigikeyScraper.getResult(null,
+			// 'http://www.digikey.tw/product-detail/zh/comchip-technology/ZENER-KIT/641-1426-ND/2217259'
+			// );
+			// await Product.create(result);
+
+			let products = await Product.find();
 			return res.ok({
 				success: true,
-				results: results
+				results: products
 			});
 		} catch (e) {
 			return res.serverError(e);
 		}
 	},
 	create: async(req, res) => {
-		let data = req.body;
+		let {
+			html, url, uuid, ip
+		} = req.body;
 
 		try {
 			let results = [];
-			let result = await DigikeyScraper.getResult(data.html, data.url);
-			results.push(result);
-			return res.ok({
-				success: true,
-				results: results
-			});
+			let result = await DigikeyScraper.getResult(html, url);
+			result.url = url;
+			result.ip = ip;
+			result.uuid = uuid;
+
+			if (result.sku) {
+				let product = await Product.create(result);
+				results.push(result);
+
+				return res.ok({
+					success: true,
+					results: product
+				});
+			} else {
+				let parseError = await ParseError.create({
+					url: url,
+					ip: ip,
+					uuid: uuid
+				});
+				await parseError.save();
+				return res.ok({
+					success: false,
+					results: parseError
+				});
+			}
 		} catch (e) {
 			return res.serverError(e);
 		}
