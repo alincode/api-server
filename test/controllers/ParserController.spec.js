f26eac707604fb7312cf8725a95a07e840a3d15d
alincode <alincode@gmail.com>
2016-11-09 16:27:40 +0800

    batch api unit test

--- test/controllers/ParserController.spec.js
+++ test/controllers/ParserController.spec.js
@@ -33,4 +33,32 @@ describe('ParserController', () => {
       done(e);
     }
   });
+
+  it.only('POST /api/v1/batch/parsers', (done) => {
+
+    let formData = {
+      ip: '104.104.104.104',
+      uuid: '00000xxxxx0000',
+      data: [{
+        'url': 'http://www.digikey.tw/product-detail/zh/comchip-technology/ZENER-KIT/641-1426-ND/2217259'
+      }, {
+        'url': 'http://www.digikey.tw/product-detail/zh/fairchild-semiconductor/DIODESKITFS/DIODESKITFS-ND/1770760'
+      }, {
+        'url': 'http://www.digikey.tw/product-detail/zh/micro-commercial-co/353-8001-KIT/353-8001-KIT-ND/1372186'
+      }]
+    };
+
+    try {
+      request(sails.hooks.http.app)
+        .post('/api/v1/batch/parsers')
+        .send(formData)
+        .end((err, res) => {
+          let result = res.body;
+          res.statusCode.should.be.equal(200);
+          done(err);
+        });
+    } catch (e) {
+      done(e);
+    }
+  });
 });
