5897ccb111a75711cfd860144da720dd70cd3faf
alincode <alincode@gmail.com>
2016-11-11 12:16:08 +0800

    update test case

--- test/controllers/ParserController.spec.js
+++ test/controllers/ParserController.spec.js
@@ -1,25 +1,11 @@
 describe('ParserController', () => {
 
-  it('GET /api/v1/parsers', (done) => {
-    try {
-      request(sails.hooks.http.app)
-        .get('/api/v1/parsers')
-        .end((err, res) => {
-          let result = res.body;
-          res.statusCode.should.be.equal(200);
-          done(err);
-        });
-    } catch (e) {
-      done(e);
-    }
-  });
-
   it('POST /api/v1/parsers', (done) => {
 
     let formData = {
       ip: '104.104.104.104',
       uuid: '00000xxxxx0000',
-      'url': 'http://www.digikey.tw/product-detail/zh/comchip-technology/ZENER-KIT/641-1426-ND/2217259'
+      url: 'http://www.digikey.tw/product-detail/zh/comchip-technology/ZENER-KIT/641-1426-ND/2217259'
     };
 
     try {
@@ -53,6 +39,20 @@ describe('ParserController', () => {
       request(sails.hooks.http.app)
         .post('/api/v1/batch/parsers')
         .send(formData)
+        .end((err, res) => {
+          let result = res.body;
+          res.statusCode.should.be.equal(200);
+          done(err);
+        });
+    } catch (e) {
+      done(e);
+    }
+  });
+
+  it('GET /api/v1/parsers', (done) => {
+    try {
+      request(sails.hooks.http.app)
+        .get('/api/v1/parsers')
         .end((err, res) => {
           let result = res.body;
           res.statusCode.should.be.equal(200);
