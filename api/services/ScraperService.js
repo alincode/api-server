e0dcbb9f21168c3a2b20ff13a61285d7491accfb
alincode <alincode@gmail.com>
2016-11-14 19:15:07 +0800

    add mouser scraper

--- api/services/ScraperService.js
+++ api/services/ScraperService.js
@@ -1,5 +1,6 @@
 const DigikeyTw = require('digikey-tw-scraper').default;
 const DigikeyCn = require('digikey-cn-scraper').default;
+const Mouser = require('mouser-scraper').default;
 
 // DigikeyScraper
 let self = module.exports = {
@@ -9,11 +10,14 @@ let self = module.exports = {
     return result;
   },
   getStrategy: (html, url) => {
+    // TODO: implement it, maybe like map<sting url, new object>
     let grabStrategy;
     if (url.indexOf('www.digikey.tw') != -1) {
       grabStrategy = new DigikeyTw(html, url);
     } else if (url.indexOf('www.digikey.com.cn') != -1) {
       grabStrategy = new DigikeyCn(html, url);
+    } else if (url.indexOf('www.mouser.cn') != -1) {
+      grabStrategy = new Mouser(html, url);
     }
     return grabStrategy;
   }
