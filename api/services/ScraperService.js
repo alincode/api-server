09ae6956cdcdbf036b4a352dde697cef861f7ba9
alincode <alincode@gmail.com>
2016-11-16 19:20:10 +0800

    add chip1stop-scraper

--- api/services/ScraperService.js
+++ api/services/ScraperService.js
@@ -1,7 +1,7 @@
 const DigikeyTw = require('digikey-tw-scraper').default;
 const DigikeyCn = require('digikey-cn-scraper').default;
 const Mouser = require('mouser-scraper').default;
-
+const Chip1stop = require('chip1stop-scraper').default;
 // DigikeyScraper
 let self = module.exports = {
   getResult: async(html, url) => {
@@ -18,6 +18,8 @@ let self = module.exports = {
       grabStrategy = new DigikeyCn(html, url);
     } else if (url.indexOf('www.mouser.cn') != -1) {
       grabStrategy = new Mouser(html, url);
+    } else if (url.indexOf('www.chip1stop.com') != -1) {
+      grabStrategy = new Chip1stop(html, url);
     }
     return grabStrategy;
   }
