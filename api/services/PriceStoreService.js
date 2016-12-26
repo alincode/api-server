0d368b8b43e9785dfea85d312b49cb23a09cc3fb
alincode <alincode@gmail.com>
2016-12-26 14:17:38 +0800

    implement PriceStoreService

---
+++ api/services/PriceStoreService.js
@@ -0,0 +1,27 @@
+// PriceStoreService
+module.exports = {
+  create: async(grabStore, productId) => {
+    let data = [];
+    try {
+      if (grabStore.priceStores) {
+        grabStore.priceStores.forEach((priceStore) => {
+          let newPriceStore = {
+            amount: priceStore.amount,
+            unitPrice: priceStore.unitPrice,
+            supplierId: parseInt(grabStore.supplierId),
+            sku: grabStore.sku,
+            mfs: grabStore.mfs,
+            pn: grabStore.pn
+          };
+          if (productId) newPriceStore.productId = productId;
+          data.push(newPriceStore);
+        });
+      }
+      await PriceStore.create(data);
+      return true;
+    } catch (e) {
+      sails.log.error(e);
+      return false;
+    }
+  }
+};
